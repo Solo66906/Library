20c97f88f4bfb2d67fcbf07b7ec372a8c98f1a75
Solo66906 <53848126+Solo66906@users.noreply.github.com>
2024-02-18 21:49:34 +0500

    create slider

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('.slider').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  })
+})
